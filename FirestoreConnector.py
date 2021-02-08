0d9f75321c904a4c053146bdbdd4424222a4230e
Felix Gundlack <GundlackFelixDev@gmail.com>
2021-02-08 13:49:02 +0100

    allow multiple instance to open different project connections

--- FirestoreConnector.py
+++ FirestoreConnector.py
@@ -11,13 +11,17 @@ class FirestoreConnector():
     def __init__(self, project, path_cred):
         assert isinstance(project, str)
         assert isinstance(path_cred, str)
-        assert os.path.isfile(path_cred), f"Invalide credential file_path: [{path_cred}] does not exist!"
+        assert os.path.isfile(
+            path_cred), f"Invalide credential file_path: [{path_cred}] does not exist!"
         self.cred = credentials.Certificate(path_cred)
         self.url = f"https://{project}.firebaseio.com"
-
-        self.app = firebase_admin.initialize_app(
-            self.cred, {'databaseURL': self.url})
-        self.db = firestore.client()
+        if not len(firebase_admin._apps):
+            self.app = firebase_admin.initialize_app(
+                self.cred, {'databaseURL': self.url})
+        else:
+            self.app = firebase_admin.initialize_app(
+                self.cred, {'databaseURL': self.url}, project)
+        self.db = firestore.client(self.app)
 
     def get_document(self, name, _id):
         doc_ref = self.db.collection(name).document(_id)
@@ -36,8 +40,8 @@ class FirestoreConnector():
     def update_collection(self, name, data):
         assert '_id' in data.keys()
         doc_ref = self.db.collection(name).document(data['_id'])
-        data['changed'] = str(datetime.now())
         if not self.check_exists(name, data):
             data['created'] = str(datetime.now())
+        data['changed'] = str(datetime.now())
 
         doc_ref.set(data, merge=True)
